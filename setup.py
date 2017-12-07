306c8c3c2a43febd0ba7cf1e191b171c7a50be82
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-12-06 19:01:54 -0600

    Basic project setup

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import itertools
+
+# Parse the version from the main __init__.py
+with open('madmex/__init__.py') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+# 
+extra_reqs = {'docs': ['sphinx',
+                       'sphinx-rtd-theme',
+                       'sphinxcontrib-programoutput']}
+extra_reqs['all'] = list(set(itertools.chain(*extra_reqs.values())))
+
+setup(name='madmex',
+      version=version,
+      description=u"Scalable production of land cover and land cover change information",
+      classifiers=[],
+      keywords='Landsat, data cube, sentinel, rapidEye, Land cover',
+      author=u"Amaury, Eric, Loic, Roberto",
+      author_email='',
+      url='https://github.com/CONABIO/antares3.git',
+      license='GPLv3',
+      packages=find_packages(),
+      install_requires=[
+          'rasterio',
+          'datacube',
+          'xarray',
+          'psycopg2']
+      scripts=[],
+      test_suite="tests",
+      extras_require=extra_reqs)
+
