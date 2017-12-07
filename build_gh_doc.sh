9c01f274419986efeeb88ddd05d447c94ebb443f
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-12-07 15:49:11 -0600

    Setup for automatic compilation of online doc

---
+++ build_gh_doc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Remove everything except this script
+find . -type f -not -name 'compile-gh-doc.sh' -not -path '*/\.*' -print0 | xargs -0 rm --
+rm -R -- */
+# Checkout whatever is necessary to compile the doc (from master branch)
+git checkout master docs madmex 
+git reset HEAD
+cd docs
+make html
+cd ..
+mv -f docs/_build/html/* .
+rm -rf madmex docs
+git add -A
+git commit -m "Generated doc for gh-pages for `git log master -1 --pretty=short --abbrev-commit`"
+git push origin gh-pages
+
