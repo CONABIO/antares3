84996cc859dcb9075704574622c42e4bc76a453f
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-12-12 12:31:27 -0600

    Override BAseCommand help string type to allow line breaks

--- madmex/management/base.py
+++ madmex/management/base.py
@@ -6,6 +6,7 @@ Created on Dec 7, 2017
 
 import logging
 import time
+from argparse import RawTextHelpFormatter
 
 from django.core.management.base import BaseCommand
 
@@ -18,6 +19,12 @@ class AntaresBaseCommand(BaseCommand):
     This class just pretends to be a decorator from django base command to provide feedback
     on how much time does a commandt takes to finish.
     '''
+    def create_parser(self, *args, **kwargs):
+        """Allow line breaks in command help string
+        """
+        parser = super(AntaresBaseCommand, self).create_parser(*args, **kwargs)
+        parser.formatter_class = RawTextHelpFormatter
+        return parser
 
     def execute(self, *args, **options):
         '''
