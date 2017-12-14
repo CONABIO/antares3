cb100936b110ee060bc0d90f2c1f1b79f4f728cf
robmartz <rmartinez@conabio.gob.mx>
2017-12-13 18:48:03 -0600

    Deploy initial interface on apolo server

--- madmex/settings.py
+++ madmex/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost')]
 
 
 # Application definition
