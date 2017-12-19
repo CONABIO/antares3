9fcd83c2a41d70223d94477539c78e3b93ca100c
amaurs <amaury.gtz@gmail.com>
2017-12-19 17:54:56 -0600

    Added ingestion of states to the database.

--- madmex/management/commands/init.py
+++ madmex/management/commands/init.py
@@ -8,7 +8,7 @@ import logging
 import os
 
 from madmex.management.base import AntaresBaseCommand
-from madmex.models import ingest_countries_from_shape
+from madmex.models import ingest_countries_from_shape, ingest_states_from_shape
 from madmex.settings import TEMP_DIR
 from madmex.util.local import aware_download, extract_zip, aware_make_dir, \
     filter_files_from_folder
@@ -31,11 +31,25 @@ class Command(AntaresBaseCommand):
         We retrieve the names given in the command line input and greet
         each one of them.
         '''
-        url = 'http://thematicmapping.org/downloads/TM_WORLD_BORDERS-0.3.zip'
+        #url = 'http://thematicmapping.org/downloads/TM_WORLD_BORDERS-0.3.zip'
+        url = 'http://data.biogeo.ucdavis.edu/data/gadm2.8/shp/%s_adm_shp.zip' % 'MEX'
         filepath = aware_download(url, TEMP_DIR)
         unzipdir = extract_zip(filepath, TEMP_DIR)
         os.listdir(unzipdir)
-        shape_name = filter_files_from_folder(unzipdir, regex=r'.*.shp')[0]
+        shape_name = filter_files_from_folder(unzipdir, regex=r'.*adm0.shp')[0]
         shape_file = os.path.join(unzipdir, shape_name)
         logger.info('This %s shape file will be ingested.' % shape_file)
-        ingest_countries_from_shape(shape_file)
+        mapping = {
+            'name' : 'ISO',
+            'the_geom' : 'MULTIPOLYGON'
+        }
+        #ingest_countries_from_shape(shape_file, mapping)
+        shape_name = filter_files_from_folder(unzipdir, regex=r'.*adm1.shp')[0]
+        shape_file = os.path.join(unzipdir, shape_name)
+        logger.info('This %s shape file will be ingested.' % shape_file)
+        mapping = {
+            'country': {'name': 'NAME_0'},
+            'name' : 'NAME_1',
+            'the_geom' : 'MULTIPOLYGON'
+        }
+        ingest_states_from_shape(shape_file, mapping)
