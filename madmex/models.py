5502314c5a39112ae4c6e67c559db015d9be837e
amaurs <amaury.gtz@gmail.com>
2018-01-19 12:55:17 -0600

    Added two tables to handle training data.

--- madmex/models.py
+++ madmex/models.py
@@ -4,8 +4,6 @@ Created on Dec 12, 2017
 @author: agutierrez
 '''
 
-from importlib import import_module
-
 from django.contrib.gis.db import models
 from django.contrib.gis.utils.layermapping import LayerMapping
 
@@ -15,7 +13,7 @@ class Country(models.Model):
     '''
     name = models.CharField(max_length=100, unique=True)
     the_geom = models.MultiPolygonField()
-    added = models.DateField(auto_now_add=True)
+    added = models.DateTimeField(auto_now_add=True)
 
 class Region(models.Model):
     '''This model represents a region that can be related to a Country. 
@@ -23,14 +21,14 @@ class Region(models.Model):
     name = models.CharField(max_length=100, unique=True)
     the_geom = models.MultiPolygonField()
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='country', default=None)
-    added = models.DateField(auto_now_add=True)
+    added = models.DateTimeField(auto_now_add=True)
 
 class Footprint(models.Model):
     '''This model represents a footprint. 
     '''
     name = models.CharField(max_length=50, unique=True)
     the_geom = models.PolygonField()
-    added = models.DateField(auto_now_add=True)
+    added = models.DateTimeField(auto_now_add=True)
     
 class Order(models.Model):
     '''This model holds the information of usgs orders. 
@@ -38,7 +36,22 @@ class Order(models.Model):
     user = models.CharField(max_length=50, default=None)
     order_id = models.CharField(max_length=100, unique=True)
     downloaded = models.BooleanField()
-    added = models.DateField(auto_now_add=True)
+    added = models.DateTimeField(auto_now_add=True)
+    
+class Tag(models.Model):
+    '''To keep a deeper control over the tags that we can handle. 
+    '''
+    name = models.CharField(max_length=50, default=None)
+
+class Training(models.Model):
+    '''This table holds objects that will be used for training. They must be related to
+    regions and each object should have an assigned tag which is the ground truth for
+    it.
+    '''
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    the_geom = models.PolygonField()
+    added = models.DateTimeField(auto_now_add=True)
+    region = models.ManyToManyField(Region)
 
 def ingest_countries_from_shape(path, mapping):
     '''Ingestion function for countries to database.
