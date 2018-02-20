1e2cfdec9502c2875dcf0a624a8e834287e84502
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-02-20 18:35:37 +0000

    Unit tests for xr.DataArray casting functions

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,10 +6,16 @@ Created on Dec 13, 2017
 import os
 import shutil
 import unittest
+from datetime import datetime
+import datetime as dt
 
 from madmex import util
+import madmex.util.xarray as xutils
 from madmex.settings import TEMP_DIR
 from madmex.util.local import aware_make_dir
+
+import numpy as np
+import xarray as xr
 
 
 class TestUtil(unittest.TestCase):
@@ -22,7 +28,7 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(util.local.basename(filename), 'file.txt')
         self.assertEqual(util.local.basename(filename, suffix=True), 'file.txt')
         self.assertEqual(util.local.basename(filename, suffix=False), 'file')
-        
+
     def test_aware_make_dir(self):
         name = os.path.join(TEMP_DIR, 'this_is_a_test')
         aware_make_dir(name)
@@ -42,6 +48,25 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(len(util.local.filter_files_from_folder(directory, r'.*\.csv')), 1)
         self.assertEqual(util.local.filter_files_from_folder(directory, r'.*\.csv')[0], 'image1.csv')
         shutil.rmtree(directory)
+
+    def test_xutils(self):
+        # Build test data
+        arr = np.array([1,2,-9999], dtype=np.int16)
+        arr_float = np.array([1,2,-9999], dtype=np.float)
+        date_list = [datetime(2018, 1, 1) + dt.timedelta(delta) for delta in range(3)]
+        xarr = xr.DataArray(arr, dims=['time'], coords={'time': date_list},
+                                                attrs={'nodata': -9999})
+        xarr_float = xr.DataArray(arr_float, dims=['time'], coords={'time': date_list},
+                                  attrs={'nodata': -9999})
+        xset_in_int = xr.Dataset({'blue': xarr, 'green': xarr, 'red': xarr})
+        xset_in_float = xr.Dataset({'blue': xarr_float, 'green': xarr_float,
+                                    'red': xarr_float})
+        # Round trip int to float (with nodata replaced by nan) and back to int
+        xset_out_float_0 = xset_in_int.apply(func=xutils.to_float, keep_attrs=True)
+        xset_out_float_1 = xset_in_float.apply(func=xutils.to_float, keep_attrs=True)
+        xset_out_int = xset_out_float_0.apply(xutils.to_int, keep_attrs=True)
+        self.assertIsNone(xr.testing.assert_equal(xset_in_int, xset_out_int))
+        self.assertIsNone(xr.testing.assert_allclose(xset_out_float_0, xset_out_float_1))
 
 if __name__ == '__main__':
     unittest.main()
