8a835fac20bda7b7a124c07c5d644950c0b84c6a
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-02-22 18:42:31 +0000

    New classmethod to monkey patch django Polygon

--- madmex/io/vector_db.py
+++ madmex/io/vector_db.py
@@ -1,5 +1,20 @@
 from . import AntaresDb
 
+from django.contrib.gis.geos import Polygon
+
+@classmethod
+def from_geobox(cls, geobox):
+    """classmethod to monkey patch django Polygon
+
+    Args:
+        geobox (datacube.GeoBox): A GeoBox (ususally an attribute of a Dataset when loaded
+            from the datacube)
+    """
+    left, bottom, right, top = geobox.geographic_extent.boundingbox
+    return cls.from_bbox((left, bottom, right, top))
+
+Polygon.from_geobox = from_geobox
+
 class VectorDb(AntaresDb):
     """docstring for VectorDb"""
     def load_from_dataset(self, table, dataset):
