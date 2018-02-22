09c83633f273cce46e0f1fed86ca3d9af8b77954
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-02-22 23:56:56 +0000

    Rasterize function for feature collection and xarray Dataset as target

--- madmex/overlay/conversions.py
+++ madmex/overlay/conversions.py
@@ -1,10 +1,32 @@
 import json
+from affine import Affine
+from rasterio.features import rasterize
 
 def rasterize_xarray(fc, dataset):
-    """
-    """
-    pass
+    """Rasterize a feature collection using an xarray dataset as target
 
+    Feature collection and xarray dataset must already be in the same CRS
+
+    Args:
+        fc (list): Collection of valid (see geojson.org) features
+        dataset (xarray.Dataset): A Dataset generated using Datacube.load() or
+            GridWorkflow.load(). Must have an affine attribute
+
+    Return:
+        numpy.array: An array with 0 as background value. Pixels overlapping
+        with features geometries are assigned the value of the geometry ID (calculated on
+        the fly based on feature order).
+    """
+    # Extract geometries from features
+    geom_list = [x['geometry'] for x in fc]
+    # Prepare iterable to be passed to rasterio rasterize
+    iterable = zip(geom_list, range(1, len(geom_list) + 1))
+    # Use Affine from the affine library to be safe
+    aff = Affine(*list(dataset.affine)[0:6])
+    # Rasterize
+    fc_raster = rasterize(iterable, transform=aff,
+                          out_shape=(dataset.sizes['y'], dataset.sizes['x']))
+    return fc_raster
 
 def rasterize_numpy(fc, arr, transform, crs):
     """
