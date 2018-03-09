2e373f9a7718bfa9f3c5f4529a97af94ce999470
amaurs <amaury.gtz@gmail.com>
2018-03-09 11:16:22 -0600

    Updated helper

--- madmex/io/helpers.py
+++ madmex/io/helpers.py
@@ -3,14 +3,14 @@ Created on Mar 7, 2018
 
 @author: agutierrez
 '''
-from madmex.models import TrainTag
+from madmex.models import Tag
 
 
-def tag_dictionary(dataset):
+def tag_dictionary(scheme):
     '''This method creates a dictionary to map the tags in the database with their
     corresponding id into a python dict.
     '''
     dictionary = {}
-    for o in TrainTag.objects.filter(key=dataset):
+    for o in Tag.objects.filter(scheme=scheme):
         dictionary[o.id] = o.value
     return dictionary
