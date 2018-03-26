7d73d9a49417c7af933c2ebb0cc31541f57bab5f
Loïc Dutrieux <loic.dutrieux@gmail.com>
2018-03-26 17:47:31 -0600

    Overlay tests

---
+++ tests/test_overlay.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+from pprint import pprint
+
+import fiona
+import xarray as xr
+import numpy as np
+from madmex.overlay.extractions import zonal_stats_pandas
+
+path = os.path.dirname(__file__)
+test_shp = os.path.join(path, 'data/test_lc_class.shp')
+test_nc = os.path.join(path, 'data/test_data.nc')
+
+dataset = xr.open_dataset(test_nc)
+with fiona.open(test_shp) as src:
+    fc = [x for x in src]
+
+
+class TestExtract(unittest.Testcase):
+    def test_extract_groupby(self):
+        X, y = zonal_stats_pandas(dataset, fc, field='class', aggregation='mean',
+                                  categorical_variables='cover')
+        expected_X = np.array([4., 22., 1],
+                              [23., 3., 2])
+        self.assertListEqual(y, ['water', 'forest'])
+        np.testing.assert_allclose(X, expected_X)
+
+
