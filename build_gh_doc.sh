0025124da718ef5ce8b139a0a2f92cb805766b26
palmoreck <palmoreck@gmail.com>
2018-04-12 12:35:44 -0500

    use branch develop instead of master

--- build_gh_doc.sh
+++ build_gh_doc.sh
@@ -3,8 +3,8 @@
 # Remove everything except this script 
 find . -type f -not -name 'build_gh_doc.sh' -not -path '*/\.*' -print0 | xargs -0 rm -- 
 rm -R -- */ 
-# Checkout whatever is necessary to compile the doc (from master branch) 
-git checkout master docs madmex  
+# Checkout whatever is necessary to compile the doc (from develop branch) 
+git checkout develop docs madmex  
 git reset HEAD 
 cd docs 
 make html 
@@ -12,5 +12,5 @@ cd ..
 mv -f docs/_build/html/* . 
 rm -rf madmex docs 
 git add -A 
-git commit -m "Generated doc for gh-pages for `git log master -1 --pretty=short --abbrev-commit`" 
+git commit -m "Generated doc for gh-pages for `git log develop -1 --pretty=short --abbrev-commit`" 
 git push origin gh-pages 
