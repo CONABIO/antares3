04ee82c8334eb72c7be45e8a2dd71a01e9e5b732
amaurs <amaury.gtz@gmail.com>
2018-05-24 15:34:33 -0500

    Added option to create style file for raster.

--- madmex/management/commands/generate_style.py
+++ madmex/management/commands/generate_style.py
@@ -8,11 +8,12 @@ import logging
 import os
 import struct
 from xml.dom.minidom import parse
-import pkg_resources as pr
 
 from madmex.management.base import AntaresBaseCommand
 from madmex.models import Tag
 from madmex.settings import TEMP_DIR
+import pkg_resources as pr
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,13 +30,16 @@ Also see the command "antares db_to_vector" for generating a vector file from da
 Example usage:
 --------------
 # Generate style file for the "madmex" classification scheme
-antares generate_style madmex --filename madmex.qml
+antares generate_style madmex --type vector --filename madmex.qml
 """
     def add_arguments(self, parser):
         parser.add_argument('scheme',
                             type=str,
                             help='Name of the classification scheme for which a style file will be generated')
-
+        parser.add_argument('--type',
+                            type=str,
+                            default='vector',
+                            help='One of vector or raster, determines the typo of style file to be written')
         parser.add_argument('--filename',
                             type=str,
                             default=None,
@@ -46,61 +50,79 @@ antares generate_style madmex --filename madmex.qml
 
     def handle(self, *args, **options):
         scheme = options['scheme']
-        filename = options['filename']
+        type = options['type']
+        filename = options['filename'] 
 
         queryset = Tag.objects.filter(scheme=scheme)
-        if queryset.count() > 0:
-            template_directory = pr.resource_filename('madmex', 'templates')
-            template = os.path.join(template_directory,'vector_style_template.qml')
-            xml_tree = parse(template)
-            categories = xml_tree.getElementsByTagName('categories')[0]
-            symbols = xml_tree.getElementsByTagName('symbols')[0]
-            for tag in queryset:
-                new_category = xml_tree.createElement('category')
-                new_category.setAttribute('render', 'true')
-                new_category.setAttribute('symbol', str(tag.numeric_code))
-                new_category.setAttribute('value', tag.value)
-                new_category.setAttribute('label', tag.value)
-                categories.appendChild(new_category )
-
-                symbol = xml_tree.createElement('symbol')
-                symbol.setAttribute('alpha', '1')
-                symbol.setAttribute('clip_to_extent', '1')
-                symbol.setAttribute('type', 'fill')
-                symbol.setAttribute('name', str(tag.numeric_code))
-
-                layer = xml_tree.createElement('layer')
-                layer.setAttribute('pass', '0')
-                layer.setAttribute('class', 'SimpleFill')
-                layer.setAttribute('locked', '0')
-
-                def create_prop(dom, key, value):
-                    prop = dom.createElement('prop')
-                    prop.setAttribute('k', key)
-                    prop.setAttribute('v', value)
-                    return prop
-                color = tag.color
-
-                if color.startswith('#'):
-                    color = color[1:]
-
-                rgb = struct.unpack('BBB', bytes.fromhex(color))
-                layer.appendChild(create_prop(xml_tree, 'border_width_map_unit_scale', '0,0,0,0,0,0'))
-                layer.appendChild(create_prop(xml_tree, 'color', '%s,%s,%s,255' % rgb))
-                layer.appendChild(create_prop(xml_tree, 'joinstyle', 'bevel'))
-                layer.appendChild(create_prop(xml_tree, 'offset', '0,0'))
-                layer.appendChild(create_prop(xml_tree, 'keyoffset_map_unit_scale', '0,0,0,0,0,0'))
-                layer.appendChild(create_prop(xml_tree, 'offset_unit', 'MM'))
-                layer.appendChild(create_prop(xml_tree, 'outline_color', '0,0,0,0'))
-                layer.appendChild(create_prop(xml_tree, 'outline_style', 'solid'))
-                layer.appendChild(create_prop(xml_tree, 'outline_width', '0.1'))
-                layer.appendChild(create_prop(xml_tree, 'outline_width_unit', 'MM'))
-                layer.appendChild(create_prop(xml_tree, 'style', 'solid'))
-                symbol.appendChild(layer)
-                symbols.appendChild(symbol)
-            if filename is None:
-                filename = os.path.join(TEMP_DIR, '%.qml' % scheme)
-            with open(filename, 'w') as file_handle:
-                xml_tree.writexml(file_handle)
+        if type == 'vector' or type == 'raster':
+            if queryset.count() > 0:
+                template_directory = pr.resource_filename('madmex', 'templates')
+                template = os.path.join(    template_directory,'%s_style_template.qml' % type)
+                xml_tree = parse(template)
+                if type == 'vector':
+                    categories = xml_tree.getElementsByTagName('categories')[0]
+                    symbols = xml_tree.getElementsByTagName('symbols')[0]
+                    for tag in queryset:
+                        new_category = xml_tree.createElement('category')
+                        new_category.setAttribute('render', 'true')
+                        new_category.setAttribute('symbol', str(tag.numeric_code))
+                        new_category.setAttribute('value', tag.value)
+                        new_category.setAttribute('label', tag.value)
+                        categories.appendChild(new_category )
+    
+                        symbol = xml_tree.createElement('symbol')
+                        symbol.setAttribute('alpha', '1')
+                        symbol.setAttribute('clip_to_extent', '1')
+                        symbol.setAttribute('type', 'fill')
+                        symbol.setAttribute('name', str(tag.numeric_code))
+    
+                        layer = xml_tree.createElement('layer')
+                        layer.setAttribute('pass', '0')
+                        layer.setAttribute('class', 'SimpleFill')
+                        layer.setAttribute('locked', '0')
+    
+                        def create_prop(dom, key, value):
+                            prop = dom.createElement('prop')
+                            prop.setAttribute('k', key)
+                            prop.setAttribute('v', value)
+                            return prop
+                        color = tag.color
+    
+                        if color.startswith('#'):
+                            color = color[1:]
+    
+                        rgb = struct.unpack('BBB', bytes.fromhex(color))
+                        layer.appendChild(create_prop(xml_tree,     'border_width_map_unit_scale', '0,0,0,0,0,0'))
+                        layer.appendChild(create_prop(xml_tree, 'color', '%s,%s,%s,255'     % rgb))
+                        layer.appendChild(create_prop(xml_tree, 'joinstyle', 'bevel'))
+                        layer.appendChild(create_prop(xml_tree, 'offset', '0,0'))
+                        layer.appendChild(create_prop(xml_tree,     'keyoffset_map_unit_scale', '0,0,0,0,0,0'))
+                        layer.appendChild(create_prop(xml_tree, 'offset_unit', 'MM'))
+                        layer.appendChild(create_prop(xml_tree, 'outline_color',    '0,0,0,0'))
+                        layer.appendChild(create_prop(xml_tree, 'outline_style',    'solid'))
+                        layer.appendChild(create_prop(xml_tree, 'outline_width', '0.1'))
+                        layer.appendChild(create_prop(xml_tree, 'outline_width_unit',   'MM'))
+                        layer.appendChild(create_prop(xml_tree, 'style', 'solid'))
+                        symbol.appendChild(layer)
+                        symbols.appendChild(symbol)
+                elif type=='raster':
+                    shader = xml_tree.getElementsByTagName('colorrampshader')[0]
+                    for tag in queryset:
+                        item = xml_tree.createElement('item')
+                        if tag.numeric_code == 0:
+                            item.setAttribute('alpha', '0')
+                        else:
+                            item.setAttribute('alpha', '255')
+                        item.setAttribute('value', str(tag.numeric_code))
+                        item.setAttribute('label', tag.value)
+                        item.setAttribute('color', tag.color)
+                        shader.appendChild(item)
+                if filename is None:
+                    filename = os.path.join(TEMP_DIR, '%s_%s.qml' % (type, scheme))
+                with open(filename, 'w') as file_handle:
+                    xml_tree.writexml(file_handle)
+                logger.error('Output written at: %s' % filename)
+            else:
+                    logger.error('The scheme does not exist.')
         else:
-            logger.error('The scheme does not exist.')
+            logger.error('Not a valid type: %s' % type)
---
+++ madmex/templates/raster_style_template.qml
@@ -0,0 +1,16 @@
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+<qgis version="2.14.19-Essen" minimumScale="-4.65661e-10" maximumScale="1e+08" hasScaleBasedVisibilityFlag="0">
+  <pipe>
+    <rasterrenderer opacity="1" alphaBand="0" classificationMax="33" classificationMinMaxOrigin="User" band="1" classificationMin="0" type="singlebandpseudocolor">
+      <rasterTransparency/>
+      <rastershader>
+        <colorrampshader colorRampType="INTERPOLATED" clip="0">
+        </colorrampshader>
+      </rastershader>
+    </rasterrenderer>
+    <brightnesscontrast brightness="0" contrast="0"/>
+    <huesaturation colorizeGreen="128" colorizeOn="0" colorizeRed="255" colorizeBlue="128" grayscaleMode="0" saturation="0" colorizeStrength="100"/>
+    <rasterresampler maxOversampling="2"/>
+  </pipe>
+  <blendMode>0</blendMode>
+</qgis>
