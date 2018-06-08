ebae663f4e32c0e7b3b47df3d305597b02afbf28
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-06-08 21:16:13 +0000

    Command line to run validation

---
+++ madmex/management/commands/validate.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+"""
+Author: Loic Dutrieux
+Date: 2018-06-08
+Purpose: Compute validation metrics on an existing object based classification
+"""
+from madmex.management.base import AntaresBaseCommand
+import logging
+
+from madmex.validation import validate, query_validation_intersect
+
+logger = logging.getLogger(__name__)
+
+class Command(AntaresBaseCommand):
+    help = """
+Compute validation metrics over an existing object based classification and optionally
+log it the database.
+
+Both the classification and the validation data must be present in the database
+See the antares ingest_validation for instruction on how to ingest a validation dataset
+
+--------------
+Example usage:
+--------------
+# Validate map, and log the result to the database
+antares validate --classification chihuahua_nalcm_2015 --validation bits_interpret --region Chihuahua --log
+"""
+    def add_arguments(self, parser):
+        parser.add_argument('-c', '--classification',
+                            type=str,
+                            required=True,
+                            help='Name of the classification to validate')
+        parser.add_argument('-val', '--validation',
+                            type=str,
+                            required=True,
+                            help='Name of the validation dataset to use to perform the validation')
+        parser.add_argument('-r', '--region',
+                            type=str,
+                            default=None,
+                            help=('Optional name of a region to use for spatially constraining the validation. '
+                                  'If left empty validation is ran over the full extent of the validation dataset. '
+                                  'The geometry of the region should be present in the madmex-region or the madmex-country table of the database. '
+                                  'Use ISO country code for country name'))
+        parser.add_argument('--log',
+                            action='store_true',
+                            help='Write the validation metrics to the database when specified.')
+
+    def handle(self, *args, **options):
+        # Unpack variables
+        classification = options['classification']
+        validation = options['validation']
+        region = options['region']
+        log = options['log']
+
+        # Query the data
+        fc_valid, fc_test = query_validation_intersect(validation_set=validation,
+                                                       test_set=classification,
+                                                       region=region)
+
+        # Run the validation
+        labels, conf = validate(fc_valid, fc_test)
+        print(labels)
+        print(conf)
+
+        # Optionally log the results to the db
+        if log:
+            logger.info('Logging the results of a validation hasn\'t been implemented yet')
+
