e224083fcc6c9c6f61e0aceb7d562858fcf53c7b
Loic Dutrieux <loic.dutrieux@gmail.com>
2018-07-10 21:05:29 +0000

    New glob like function for s3

--- madmex/util/s3.py
+++ madmex/util/s3.py
@@ -1,4 +1,5 @@
 import os
+import re
 try:
     import boto3
 except ImportError:
@@ -7,12 +8,20 @@ else:
     _has_boto3 = True
 
 
-def list_folders(bucket, path):
+def list_folders(bucket, path, pattern=None):
     """List 'sub-folders' of a s3 bucket 'folder'
 
     Args:
         bucket (str): Name of an existing s3 bucket
-        path (str): Path of the folder to containing the sub-folders to be listed
+        path (str): Path of the folder containing the sub-folders to be listed
+        pattern (str): Optional regex like pattern to filter the returned directories
+
+    Examples:
+        >>> from madmex.util import s3
+        >>> # List all Landsat 8 scenes of conabio-s3-oregon bucket
+        >>> s3.list_folders('conabio-s3-oregon', 'linea_base/L8/')
+        >>> # Filter 039/037 path/row
+        >>> s3.list_folders('conabio-s3-oregon', 'linea_base/L8/', pattern=r'.*LC08039037.*')
 
     Returns:
         list: List of subfolder names
@@ -29,4 +38,43 @@ def list_folders(bucket, path):
     out_list = []
     for page in page_iterator:
         out_list += [x.get('Prefix') for x in page.get('CommonPrefixes')]
+    if pattern is not None:
+        pattern = re.compile(pattern)
+        out_list = [x for x in out_list if pattern.search(x)]
     return out_list
+
+
+def list_files(bucket, path, pattern=None):
+    """List objects within a 'path' in an s3 bucket
+
+    Note that objects are listed recursively
+
+    Args:
+        bucket (str): Name of an existing s3 bucket
+        path (str): Path of the folder containing the objects to be listed
+        pattern (str): Optional regex like pattern to filter the returned objects
+
+    Examples:
+        >>> from madmex.util import s3
+        >>> # List all files of a Landsat directory
+        >>> s3.list_files(bucket='conabio-s3-oregon', path='linea_base/L8/LC080190482017022301T1-SC20180611232224')
+        >>> # Filter metadata file
+        >>> s3.list_files(bucket='conabio-s3-oregon', path='linea_base/L8/LC080190482017022301T1-SC20180611232224',
+        ...               pattern=r'.*.xml$')
+
+    Return:
+        list: List of s3 keys (objects)
+    """
+    if not _has_boto3:
+        raise ImportError('boto3 is required for working with s3 buckets')
+    # Strip leading and trailing slash
+    path = path.strip('/')
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket(bucket)
+    obj_list = my_bucket.objects.filter(Prefix=path)
+    out = [x.key for x in obj_list]
+    if pattern is not None:
+        pattern = re.compile(pattern)
+        out = [x for x in out if pattern.search(x)]
+    return out
+
