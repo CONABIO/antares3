4977aebed8bf5db64faa0a063bde20bd9b1a9cdf
Erick Palacios Moreno <palmoreck@gmail.com>
2018-10-04 12:06:00 -0500

    adding s2_10m_001 for RECIPES dictionary

--- madmex/recipes/__init__.py
+++ madmex/recipes/__init__.py
@@ -4,6 +4,7 @@ from .landsat_madmex_002 import run as landsat_madmex_002
 from .landsat_colombia_001 import run as landsat_colombia_001
 from .landsat_ndvi_mean import run as landsat_ndvi_mean
 from .s2_20m_001 import run as s2_20m_001
+from .s2_10m_001 import run as s2_10m_001
 
 """
 Adding recipes:
@@ -44,4 +45,9 @@ RECIPES = {
         'fun': s2_20m_001,
         'config_file': os.path.expanduser('~/.config/madmex/indexing/s2_20m_001.yaml'),
     },
+    's2_10m_001: {
+        'product': ['s2_l2a_10m_mexico'],
+        'fun': s2_10m_001,
+        'config_file': os.path.expanduser('~/.config/madmex/indexing/s2_10m_001.yaml'),
+    }
 }
