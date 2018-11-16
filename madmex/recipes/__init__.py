7098f7d787159b50f2cda404ca9dfeb58ecbe594
Erick Palacios Moreno <palmoreck@gmail.com>
2018-11-16 13:17:57 -0600

    doing some cleaning

--- madmex/recipes/__init__.py
+++ madmex/recipes/__init__.py
@@ -6,7 +6,6 @@ from .landsat_belize_001 import run as landsat_belize_001
 from .landsat_guyana_001 import run as landsat_guyana_001
 from .landsat_ndvi_mean import run as landsat_ndvi_mean
 from .s2_20m_001 import run as s2_20m_001
-from .s2_10m_ndvi_mean_001 import run as s2_10m_ndvi_mean_001
 from .s2_10m_scl_ndvi_mean_001 import run as s2_10m_scl_ndvi_mean_001
 from .s1-2_10m_001 import run as s1-2_10m_001
 
@@ -63,11 +62,6 @@ RECIPES = {
         'product': ['s2_l2a_20m_s3_mexico'],
         'fun': s2_20m_001,
         'config_file': os.path.expanduser('~/.config/madmex/indexing/s2_20m_s3_001.yaml'),
-    },
-    's2_10m_ndvi_mean_001': {
-        'product': ['s2_l2a_10m_mexico'],
-        'fun': s2_10m_ndvi_mean_001,
-        'config_file': os.path.expanduser('~/.config/madmex/indexing/s2_10m_ndvi_mean_001.yaml'),
     },
     's2_10m_scl_ndvi_mean_001': {
         'product': ['s2_l2a_10m_scl_s3_mexico'],
