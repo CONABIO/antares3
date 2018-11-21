6715e671390973cc01b13ed9467dde5d05553731
Erick Palacios Moreno <palmoreck@gmail.com>
2018-11-20 19:41:20 -0600

    returning to 5000 in dask chunks

--- madmex/recipes/s2_10m_scl_ndvi_mean_001.py
+++ madmex/recipes/s2_10m_scl_ndvi_mean_001.py
@@ -36,7 +36,7 @@ def run(tile, center_dt, path):
         if os.path.isfile(nc_filename):
             logger.warning('%s already exists. Returning filename for database indexing', nc_filename)
             return nc_filename
-        sr_0 = xr.merge([GridWorkflow.load(x, dask_chunks={'x': 3000, 'y': 3000},
+        sr_0 = xr.merge([GridWorkflow.load(x, dask_chunks={'x': 5000, 'y': 5000},
 					   measurements=['red','nir','pixel_qa']) for x in tile[1]])
         sr_0.attrs['geobox'] = tile[1][0].geobox
         sr_0 = sr_0.apply(func=to_float, keep_attrs=True)
