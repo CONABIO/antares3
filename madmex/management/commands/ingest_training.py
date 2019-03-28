c379d743fd851391ad34b15736c8176dcb3f7f0a
ixime <ixmezach@googlemail.com>
2019-03-28 16:03:59 -0600

    add random sampling per class from a raster as input file

--- madmex/management/commands/ingest_training.py
+++ madmex/management/commands/ingest_training.py
@@ -3,28 +3,31 @@
 """
 Author: Loic Dutrieux
 Date: 2018-08-30
-Purpose: Ingest a vector file containing training data into the antares database
+Purpose: Ingest a subset of training data randomly sampled from a raster into the antares database
 """
 
 import logging
 import json
 import os
+import numpy as np
+import random
 
 from django.contrib.gis.geos.geometry import GEOSGeometry
-import fiona
+import rasterio
+import rasterio.features
+from shapely.geometry import mapping, shape
 from fiona.crs import to_string
 from pyproj import Proj
 
 from madmex.management.base import AntaresBaseCommand
 from madmex.models import TrainObject, Tag, TrainClassification
-from madmex.util.spatial import feature_transform
+from madmex.util.spatial import geometry_transform
 
 logger = logging.getLogger(__name__)
 
 class Command(AntaresBaseCommand):
     help = """
-Ingest a vector file containing training data into the antares database
-
+Ingest a subset of training data randomly sampled from a raster into the antares database
 --------------
 Example usage:
 --------------
@@ -34,6 +37,9 @@ antares ingest_training /path/to/file.shp --scheme madmex --year 2015 --name tra
         parser.add_argument('input_file',
                             type=str,
                             help='Path of vector file to ingest')
+        parser.add_argument('-frac', '--fraction',
+                            type=float,
+                            help='fraction of data to ingest. If negative, ~310000 polygons will be ingested')
         parser.add_argument('-scheme', '--scheme',
                             type=str,
                             help='Name of the classification scheme to which the data belong')
@@ -54,19 +60,57 @@ antares ingest_training /path/to/file.shp --scheme madmex --year 2015 --name tra
         scheme = options['scheme']
         field = options['field']
         name = options['name']
+        
+        def add_samples(frac, arr, train_arr):
+            mask = np.zeros(train_arr.size, dtype=np.uint8)
+            for indx in np.where(arr)[0]:
+                tmp = np.zeros(train_arr.shape, dtype=np.uint8)
+                tmp[train_arr == indx+1] = 1
+                ind = np.where(tmp.reshape(-1) == 1)[0]
+                num = int(np.ceil(ind.size*frac))  # 0.7/3600
+                samples = np.array(random.sample(list(ind),num))
+                mask[samples] = 1
+            mask = mask.reshape(train_arr.shape)
+            return mask
+
         # Create ValidClassification objects list
         # Push it to database
-
+        
         # Read file and Optionally reproject the features to longlat
-        with fiona.open(input_file) as src:
+        with rasterio.open(input_file) as src:
+          try:
+            train_arr = src.read(1)
+            aff = src.transform
+            #crs_str = to_string(src.crs)
+
+            # Build mask for shapes
+            mask = np.zeros(train_arr.shape, dtype=np.uint8)
+            # Count total number of pixels per class
+            pxpcl = np.array([train_arr[train_arr == cl].size for cl in range(1,32)])
+            # Set fraction of polygons to ingest
+            if frac < 0:
+                frac = 310000/np.sum(pxpcl)
+
+            # Generate number of samples per class
+            smplpxpcl = np.array([int(np.ceil(train_arr[train_arr == cl].size*frac)) for cl in range(1,32)])
+
+            # Generate mask of samples
+            smplcl = np.logical_and(pxpcl > 0, smplpxpcl > 0)
+            if any(smplcl):
+                mask[add_samples(frac, smplcl, train_arr) == 1] = 1
+
+            # Build the feature collection
             p = Proj(src.crs)
             if p.is_latlong(): # Here we assume that geographic coordinates are automatically 4326 (not quite true)
-                fc = list(src)
+                fc = [{'type': 'feature',
+                       'geometry': mapping(shape(x[0])),
+                       'properties': {'class': int(x[1])}} for x in rasterio.features.shapes(train_arr,mask,connectivity=8,transform=aff)]
             else:
                 crs_str = to_string(src.crs)
-                fc = [feature_transform(x, crs_out='+proj=longlat', crs_in=crs_str)
-                      for x in src]
-
+                fc = [{'type': 'feature',
+                       'geometry': geometry_transform(x[0], crs_out='+proj=longlat', crs_in=crs_str),
+                       'properties': {'class': int(x[1])}} for x in rasterio.features.shapes(train_arr,mask,connectivity=8,transform=aff)]
+                
         # Write features to ValidObject table
         def train_obj_builder(x):
             """Build individual ValidObjects
