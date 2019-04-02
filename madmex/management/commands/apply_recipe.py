1ccfc08e08161ed417c0b3c0f0fd4045d214cec4
Erick Palacios Moreno <palmoreck@gmail.com>
2019-04-02 16:51:12 -0600

    fixing variable name

--- madmex/management/commands/apply_recipe.py
+++ madmex/management/commands/apply_recipe.py
@@ -183,9 +183,9 @@ antares apply_recipe -recipe s1_2_10m_001 -b 2017-01-01 -e 2017-12-31 -region Ja
                 return False
         args = {'description': product_description,
                 'center_dt': center_dt,
-                'from_dt':begin,
-                'to_dt':end,
-                'algorithm':algorithm}    
+                'from_dt': begin,
+                'to_dt': end,
+                'algorithm': fun}    
         C = client.map(metadict_from_netcdf, nc_list, **args)
         r = client.gather(C)
         l_r = [add_dataset_to_db(x) for x in r]
