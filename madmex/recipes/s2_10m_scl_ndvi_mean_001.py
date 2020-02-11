e897a6ec33afcf4a738c88f530886f2ba5760343
Erick Palacios Moreno <palmoreck@gmail.com>
2020-02-11 12:47:27 -0600

    avoiding inline compute of dask

--- madmex/recipes/s2_10m_scl_ndvi_mean_001.py
+++ madmex/recipes/s2_10m_scl_ndvi_mean_001.py
@@ -52,8 +52,7 @@ def run(tile, center_dt, path):
         sr_mean = sr_mean.rename({'ndvi': 'ndvi_mean'})
         sr_mean = sr_mean.apply(to_int)
         sr_mean.attrs['crs'] = crs
-        sr_mean = sr_mean.compute(scheduler='threads')
-        write_dataset_to_netcdf(sr_mean, nc_filename)
+        write_dataset_to_netcdf(sr_mean.compute(), nc_filename)
         # Explicitely deallocate objects and run garbage collector
         sr_0=sr_1=sr_mean=None
         gc.collect()
